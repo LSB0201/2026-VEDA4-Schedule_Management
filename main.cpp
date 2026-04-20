db47ba098873b43639f8b12922879eea67ecb7ac
Vlolet <shiningjjoon@hansung.ac.kr>
2026-04-20 16:33:03 +0900

    feat: add material library and style assets

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
+
+static bool DEBUG = true;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // stylesheet 적용
+    QFile file(":/resources/style.qss");
+    if (file.open(QFile::ReadOnly | QFile::Text)){
+        QTextStream ts(&file);
+        a.setStyleSheet(ts.readAll());
+        file.close();
+        if(DEBUG){
+            qDebug() << "style sheet load";
+        }
+    }
+
     MainWindow w;
     w.show();
     return QCoreApplication::exec();
